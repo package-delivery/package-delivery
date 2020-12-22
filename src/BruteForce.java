6f1950f9574091dd1eaab4876a84322ad340fa97
Kurisu003 <74920220+Kurisu003@users.noreply.github.com>
2020-12-22 23:53:58 +0100

    Updated BruteForce class
    Updated BruteForce class to implement the beginning of the BruteForce class. One small step for man ... one huge leap for mankind!

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,6 +1,39 @@
+import java.util.*;
+
 public class BruteForce implements Algorithm{
+
+    //1 has to be added to the initialisation because we need to return home
+    private static City [] citiesArray = new City[CsvReader.getCityMatrix().length + 1];
+    private Cities testCity = new Cities();
+
     @Override
-    public Cities getResult() {
-        return null;
+    public Cities getResult(){
+
+        double distance = 0;
+        double test = 0;
+
+
+        //Writes each City object to matching part in array
+        for(int i = 0; i < CsvReader.getCityMatrix().length; i++){
+            citiesArray[i] = CsvReader.getCityMatrix()[i];
+        }
+
+        //sets starting point to last place in array, so the distance home gets returned too
+        citiesArray[CsvReader.getCityMatrix().length] = CsvReader.getCityMatrix()[0];
+
+
+        //to add: break if curr path is longer that old path
+        for(int i = 0; i < citiesArray.length - 1; i++){
+            distance += CsvReader.getDistanceMatrix()[citiesArray[i].getId()][citiesArray[i + 1].getId()];
+        }
+
+
+
+        
+
+        return testCity;
+
     }
 }
+
+
