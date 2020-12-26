2cc7f48e6bc1f13a74e4047dbd95fd53dd6f2e2a
Kurisu003 <74920220+Kurisu003@users.noreply.github.com>
2020-12-26 20:11:59 +0100

    Updated BruteForce class
    Updated BruteForce class to actually work, even if not well. Code still has to be improved and cleaned up.

--- src/main/java/com/packagedelivery/BruteForce.java
+++ src/main/java/com/packagedelivery/BruteForce.java
@@ -1,6 +1,11 @@
 package com.packagedelivery;
+import java.util.ArrayList; // import the ArrayList class
 
 public class BruteForce implements Algorithm{
+
+    private static ArrayList<String> permutationsArray = new ArrayList<String>();
+
+    //to add: break if curr path is longer that old path
 
     //1 has to be added to the initialisation because we need to return home
     private static City [] citiesArray = new City[CsvReader.getCityMatrix().length + 1];
@@ -8,7 +13,7 @@ public class BruteForce implements Algorithm{
 
     private static void permutation(String prefix, String str) {
         int n = str.length();
-        if (n == 0) System.out.println(prefix);
+        if (n == 0) permutationsArray.add(prefix); //System.out.println(prefix);
         else {
             for (int i = 0; i < n; i++)
                 permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
@@ -26,27 +31,48 @@ public class BruteForce implements Algorithm{
         //Writes each City object to matching part in array
         for(int i = 0; i < CsvReader.getCityMatrix().length; i++){
             citiesArray[i] = CsvReader.getCityMatrix()[i];
-            testString += CsvReader.getCityMatrix()[i].getCityName();
+            testString += CsvReader.getCityMatrix()[i].getId();
         }
 
         //sets starting point to last place in array, so the distance home gets returned too
         citiesArray[CsvReader.getCityMatrix().length] = CsvReader.getCityMatrix()[0];
-        testString += CsvReader.getCityMatrix()[0].getCityName();
+        testString += CsvReader.getCityMatrix()[0].getId();
 
-
-        //to add: break if curr path is longer that old path
         //loops through all cities and adds length together
-        for(int i = 0; i < citiesArray.length - 1; i++){
-            distance += CsvReader.getDistanceMatrix()[citiesArray[i].getId()][citiesArray[i + 1].getId()];
-        }
-
-        //for(int i = 0; i < citiesArray.length; i++)
-        //System.out.println(citiesArray[i].getCityName());
+        //for(int i = 0; i < citiesArray.length - 1; i++){
+          //  distance += CsvReader.getDistanceMatrix()[citiesArray[i].getId()][citiesArray[i + 1].getId()];
+        //}
 
         //to remove first and last position from teststring
-        testString = testString.substring(1, testString.length()-1);
 
-        permutation("", testString);
+        permutation("", testString.substring(1, testString.length()-1));
+
+        for(int i = 0; i < permutationsArray.size(); i++){
+            //System.out.println(permutationsArray.get(i));
+        }
+
+        //to add "home" to permutationsArray
+        for(int i = 0; i < permutationsArray.size(); i++){
+            permutationsArray.set(i, testString.charAt(0) + permutationsArray.get(i) + testString.charAt(0));
+            //System.out.println(testString.charAt(0) + permutationsArray.get(i) + testString.charAt(0));
+        }
+
+
+        for(int i = 0; i < permutationsArray.size(); i++){
+            //System.out.println(permutationsArray.get(i));
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for(int i = 0; i < permutationsArray.size(); i++){
+            System.out.println(permutationsArray.get(i));
+            for(int j = 0; j < permutationsArray.get(i).length() - 1; j++){
+                distance += CsvReader.getDistanceMatrix()[Integer.parseInt(String.valueOf(permutationsArray.get(i).charAt(j)))][Integer.parseInt(String.valueOf(permutationsArray.get(i).charAt(j + 1)))];
+            }
+            System.out.println(distance);
+            distance = 0;
+        }
 
         return testCity;
 
