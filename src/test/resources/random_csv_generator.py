ef5cd65025c367720e54e064cd55ac63918727d3
zLuki <61950131+zLuki@users.noreply.github.com>
2021-01-09 23:25:53 +0100

    Finished python script
    Py script for creating random csv files is finished.

--- src/test/resources/random_csv_generator.py
+++ src/test/resources/random_csv_generator.py
@@ -3,10 +3,41 @@
 import random
 
 cities = ["Albany", "Amsterdam", "Auburn", "Babylon", "Batavia", "Beacon", "Bedford", "Binghamton", "Bronx", "Brooklyn", "Buffalo", "Chautauqua", "Cheektowaga", "Clinton", "Cohoes", "Coney Island", "Cooperstown", "Corning", "Cortland", "Crown Point", "Dunkirk", "East Aurora", "East Hampton", "Eastchester", "Elmira", "Flushing", "Forest Hills", "Fredonia", "Garden City", "Geneva", "Glens Falls", "Gloversville", "Great Neck", "Hammondsport", "Harlem", "Hempstead", "Herkimer", "Hudson", "Huntington", "Hyde Park", "Ilion", "Ithaca", "Jamestown", "Johnstown", "Kingston", "Lackawanna", "Lake Placid", "Levittown", "Lockport", "Mamaroneck", "Manhattan", "Massena", "Middletown", "Mineola", "Mount Vernon", "New Paltz", "New Rochelle", "New Windsor", "New York City", "Newburgh", "Niagara Falls", "North Hempstead", "Nyack", "Ogdensburg", "Olean", "Oneida", "Oneonta", "Ossining", "Oswego", "Oyster Bay", "Palmyra", "Peekskill", "Plattsburgh", "Port Washington", "Potsdam", "Poughkeepsie", "Queens", "Rensselaer", "Rochester", "Rome", "Rotterdam", "Rye", "Sag Harbor", "Saranac Lake", "Saratoga Springs", "Scarsdale", "Schenectady", "Seneca Falls", "Southampton", "Staten Island", "Stony Brook", "Stony Point", "Syracuse", "Tarrytown", "Ticonderoga", "Tonawanda", "Troy", "Utica", "Watertown", "Watervliet", "Watkins Glen", "West Seneca", "White Plains", "Woodstock", "Yonkers"]
-print (len(cities))
 
-for i in range(100):
+
+for w in range(100):
+    matrix = [["CSV"]]
     random.shuffle(cities)
-    s = "CSV," + ','.join(cities)
-    print(s)
+    l = random.randint(0, 15)
 
+    # add cities
+    for x in range(l):
+        matrix[0].append(cities[x])
+
+    # add cities and fill with empty strings
+    for x in range(l):
+        temp = []
+        temp.append(cities[x])
+        for j in range(l):
+            temp.append("")
+        matrix.append(temp)
+
+    # add values to matrix
+    for i in range(1, len(matrix)):
+        for x in range(len(matrix)-1, 0, -1):
+            if x > i:
+                matrix[i][x] = str(round(random.random()*100, 1))
+            elif x == i:
+                matrix[i][x] = "0"
+            else:
+                matrix[i][x] = matrix[x][i]
+
+    # Write to file
+    finalstring = []
+    for x in matrix:
+        finalstring.append(','.join(x))
+    finalstring = '\n'.join(finalstring)
+    filename = "zTSP_"+str(w)+".csv"
+    f = open(filename, "w")
+    f.write(finalstring)
+    f.close()
