abda4d0c3858d0367b88783e97ae119dc849fddb
Lukas Schatzer <Schatzer.Lukas@gmail.com>
2021-01-25 23:56:19 +0100

    Finished convex hull

--- src/main/java/com/packagedelivery/ConvexHull.java
+++ src/main/java/com/packagedelivery/ConvexHull.java
@@ -1,45 +1,84 @@
 package com.packagedelivery;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.TreeMap;
 
 public class ConvexHull implements Algorithm{
+
+    class Point {
+        protected double x, y;
+
+        public Point() {}
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double getDistanceTo(Point p) {
+            return Math.sqrt(Math.pow(Math.abs(this.x-p.x), 2) + Math.pow(Math.abs(this.y-p.y), 2));
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "["+x+","+y+"]";
+        }
+    }
+
+    public Cities sortedCities;
 
     public ConvexHull(String start) {
         this(start, false);
     }
 
     public ConvexHull(String s, boolean visualization) {
-        String[] points = s.replaceAll("\\[", "").split("], ");
+
+        // start stopwatch
+        Instant starts = Instant.now();
+
+        String[] points = s.replaceAll("\\[", "").split("],");
         points[points.length-1] = points[points.length-1].replaceAll("]]", "");
-        double[][] coords = new double[points.length][2];
+
+        ArrayList<Point> coords = new ArrayList<>();
         for (int i = 0; i < points.length; i++) {
             String[] buf = points[i].split(",");
-            coords[i][0] = Double.parseDouble(buf[0]);
-            coords[i][1] = Double.parseDouble(buf[1]);
+            coords.add(new Point(Double.parseDouble(buf[0]), Double.parseDouble(buf[1])));
         }
-        double min = Double.MAX_VALUE;
-        int position = -1;
-        for (int i = 0; i < coords.length; i++) {
-            if (min > coords[i][1]) {
-                min = coords[i][1];
-                position = i;
-            }
-        }
-        TreeMap<Double, double[]> sortedPoints = new TreeMap<>();
-        sortedPoints.put(0.0, coords[position]);
-        for (int i = 0; i < coords.length; i++) {
-            if (i == position) continue;
-            double b = coords[i][0] - coords[position][0];
-            double c = coords[i][1] - coords[position][1];
+
+        Point min = new Point(0,Double.MAX_VALUE);
+        for (Point p : coords)
+            if (p.y < min.y) min = new Point(p.x, p.y);
+
+        TreeMap<Double, Point> sortedPoints = new TreeMap<>();
+        sortedPoints.put(0.0, min);
+        for (Point p : coords) {
+            if (p.equals(min)) continue;
+            double b = p.x - min.x;
+            double c = p.y - min.y;
             double erg = Math.atan(c/b);
             if (erg < 0) erg += Math.PI;
-            sortedPoints.put(erg, coords[i]);
+            sortedPoints.put(erg, p);
         }
-        ArrayList<double[]> hull = new ArrayList<>();
-        ArrayList<double[]> sortedPoints2 = new ArrayList<>();
-        for (double d : sortedPoints.keySet())
-            sortedPoints2.add(sortedPoints.get(d));
+
+        ArrayList<Point> hull = new ArrayList<>();
+        ArrayList<Point> sortedPoints2 = new ArrayList<>();
+        for (double angle : sortedPoints.keySet())
+            sortedPoints2.add(sortedPoints.get(angle));
         hull.add(sortedPoints2.get(0));
         hull.add(sortedPoints2.get(1));
         for (int i = 2; i < sortedPoints2.size(); i++) {
@@ -49,18 +88,55 @@ public class ConvexHull implements Algorithm{
             hull.add(sortedPoints2.get(i));
         }
 
-        for (int i = 0; i < hull.size(); i++) {
-            System.out.println(hull.get(i)[0]+" "+hull.get(i)[1]);
+        while (hull.size() < sortedPoints2.size()) {
+            double overallMinCost = Double.MAX_VALUE;
+            Point pointToInsert = new Point(), before = new Point(), after = new Point();
+            int insertionIndex = -1;
+            for (Point p : sortedPoints2) {
+                if (hull.contains(p)) continue;
+                double minCost = Double.MAX_VALUE;
+                for (int i = 0; i < hull.size() - 1; i++) {
+                    double currentCost = hull.get(i).getDistanceTo(p) + p.getDistanceTo(hull.get(i + 1)) - hull.get(i).getDistanceTo(hull.get(i + 1));
+                    if (currentCost < minCost) {
+                        minCost = currentCost;
+                        before = hull.get(i);
+                        after = hull.get(i + 1);
+                    }
+                }
+                double overallCurrentCost = (before.getDistanceTo(p) + p.getDistanceTo(after)) / before.getDistanceTo(after);
+                if (overallCurrentCost < overallMinCost) {
+                    overallMinCost = overallCurrentCost;
+                    pointToInsert = p;
+                    insertionIndex = hull.indexOf(after);
+                }
+            }
+            hull.add(insertionIndex, pointToInsert);
         }
+
+        // Calculate the final distance and the Cities array
+        this.sortedCities = new Cities();
+        this.sortedCities.setDistance(0.0);
+        ArrayList<City> sortedCities = new ArrayList<>();
+        for (int i = 0; i < hull.size()-1; i++) {
+            sortedCities.add(new City(hull.get(i).toString(), 0));
+            this.sortedCities.setDistance(this.sortedCities.getDistance()+hull.get(i).getDistanceTo(hull.get(i+1)));
+        }
+        sortedCities.add(new City(hull.get(hull.size()-1).toString(), 0));
+        this.sortedCities.setSortedCities(sortedCities);
+
+        // Set time
+        Instant ends = Instant.now();
+        this.sortedCities.setTime(Duration.between(starts, ends).toMillis());
     }
 
-    private boolean clock(double[] a, double[] b, double[] c) {
-        return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0]) >= 0;
+    private boolean clock(Point a, Point b, Point c) {
+        return (b.x-a.x)*(c.y-a.y) - (b.y-a.y)*(c.x-a.x) >= 0;
     }
+
 
     @Override
     public Cities getResult() {
-        return null;
+        return this.sortedCities;
     }
 
     @Override
